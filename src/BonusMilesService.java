bdb7aa7aafac2dbc62be13cd3a285723051b3f8d
Карина Миронова <Twiksyyy@gmail.com>
2022-04-27 14:30:55 +0300

    added other files

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,7 +2,7 @@ public class BonusMilesService {
     public int calculate(int cost) {
         int bonusMiles = 20;
         int miles = (cost / bonusMiles);
-        System.out.println("Вам начисленно бонусных миль: " );
+        System.out.println("Вам начисленно бонусных миль: ");
         return miles;
     }
 }
